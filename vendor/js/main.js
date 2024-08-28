d852dee0c24309f453f4f641053de954f4e0533d
Rama Omari <152034129+RamaOmari@users.noreply.github.com>
2024-08-29 00:40:37 +0300

    Add files via upload

---
+++ vendor/js/main.js
@@ -0,0 +1,27 @@
+let one = document.querySelector(".one");
+let tow = document.querySelector(".tow");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+console.log(one, tow, three, four, five, six)
+
+one.onclick= ()=> {
+    console.log("one")
+    window.location.href = "https://ramaomari.github.io/bakery/"
+}
+tow.onclick= ()=> {
+    window.location.href = "https://ramaomari.github.io/portfolio/"
+}
+three.onclick= ()=> {
+    window.location.href = "https://github.com/RamaOmari/Login-register-form/"
+}
+four.onclick= ()=> {
+    window.location.href = "https://github.com/RamaOmari/CRUD-api"
+}
+five.onclick= ()=> {
+    window.location.href = "https://github.com/RamaOmari/Fashion-magazine"
+}
+six.onclick= ()=> {
+    window.location.href = "https://github.com/RamaOmari/News-site"
+}
